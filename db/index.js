feae4b4742703d250180c6350432bfdb502483eb
ahtisham02 <ahtisham02>
2025-02-17 20:08:38 +0500

    chnges

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`mongodb+srv://Construction:kgHtlJmT4UYEaGkB@cluster0.0k3vc.mongodb.net/`)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://razorsgamer2005:admin123@cluster0.epy2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
         console.log(`\n 🚀 ~MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("❌ MONGODB connection FAILED ", error);
