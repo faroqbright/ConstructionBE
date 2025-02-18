9c7e8d6b7845a18342c866b5d7b73436d4cdbc15
ahtisham02 <ahtisham02>
2025-02-18 12:36:22 +0500

    chnges

--- models/role.model.js
+++ models/role.model.js
@@ -8,11 +8,11 @@ const roleSchema = new mongoose.Schema(
       trim: true,
       unique: true, // Ensure role names are unique
     },
-    createdBy: {
-      type: String,
-      required: true,
+    // createdBy: {
+    //   type: String,
+    //   required: true,
     
-    },
+    // },
     status: {
       type: String,
       required: true,
