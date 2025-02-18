b02d15c9f4aa528207effa080865b1e57d766609
ahtisham02 <ahtisham02>
2025-02-18 11:47:00 +0500

    chnges

--- db/index.js
+++ db/index.js
@@ -1,10 +1,25 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+
+// const connectDB = async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`mongodb+srv://razorsgamer2005:admin123@cluster0.epy2w.mongodb.net/`)
+//         console.log(`\n 🚀 ~MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+//     } catch (error) {
+//         console.log("❌ MONGODB connection FAILED ", error);
+//         process.exit(1)
+//     }
+// }
+
+// export default connectDB
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`mongodb+srv://razorsgamer2005:admin123@cluster0.epy2w.mongodb.net/`)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://Construction:kgHtlJmT4UYEaGkB@cluster0.0k3vc.mongodb.net/`)
         console.log(`\n 🚀 ~MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("❌ MONGODB connection FAILED ", error);
