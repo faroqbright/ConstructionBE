c3c1a5b097a27e0b0327e1cbb8312fd067ed7812
faroqbright <faroqbright@gmail.com>
2025-04-07 18:01:00 +0500

    chnges

--- models/CompanyModel.js
+++ models/CompanyModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { User } from "./user.model";
 
 const companySchema = new mongoose.Schema(
   {
@@ -26,5 +27,15 @@ const companySchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+companySchema.pre("deleteOne", { document: true, query: false }, async function (next) {
+  try {
+    await User.deleteMany({ companyName: this.name });
+    console.log(`Deleted all users associated with company: ${this.name}`);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 export const Company = mongoose.model("Company", companySchema);
