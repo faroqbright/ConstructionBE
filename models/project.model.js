71f23a84be85cd38fcaf66f0ea5c972080d3fb6c
Awais-789 <awais.ranjha789@gmail.com>
2025-04-18 13:12:00 +0500

    Changes

--- models/project.model.js
+++ models/project.model.js
@@ -36,11 +36,11 @@ const editProjectSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-    // description: {
-    //   type: String,
-    //   required: true,
-    //   trim: true,
-    // },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     location: {
       type: String,
       required: true,
