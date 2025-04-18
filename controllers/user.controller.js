721c966ef5e2aee1a3d749f73f5cf30feecb7e96
faroqbright <faroqbright@gmail.com>
2025-04-18 13:05:22 +0500

    smtp

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -163,12 +163,12 @@ const forgetPassword = asyncHandler(async (req, res) => {
   await user.save();
 
   const body = {
-    from: process.env.EMAIL_USER,
+    from: `"Construction Management" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: "Authentication",
     html: `<h2>Hello ${email}</h2>
         <p>Your OTP is <strong>${otp}</strong></p>
-        <p>If you did not initiate this request, please contact us immediately at eg@.com</p>
+        <p>If you did not initiate this request, please contact us immediately at app@soapro.ao</p>
         <p>Thank you</p>
         <strong>Developer Team</strong>`,
   };
