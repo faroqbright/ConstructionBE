04d2cde96e3993608e676c9f52b2ce6329e65631
Awais-789 <awais.ranjha789@gmail.com>
2025-04-23 11:28:53 +0500

    Changes

--- models/notifications.model.js
+++ models/notifications.model.js
@@ -3,20 +3,33 @@ import mongoose from "mongoose";
 const notificationsSchema = new mongoose.Schema(
   {
     title: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
       type: String,
       required: true,
       trim: true,
     },
+    type: {
+      type: String,
+      enum: ["client", "financial"],
+      required: true,
+    },
+    clientIds: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Client", // adjust if your client model has a different name  
+      default: [],
+    },
+  },
+  {
     createdAt: {
-         type: Date,
-         default: Date.now 
-        },
-},{
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
     timestamps: true,
   }
 );
