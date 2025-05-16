ed0b29709b32eadb8b1de9c57adc1a957c44fbc6
Awais-789 <awais.ranjha789@gmail.com>
2025-05-16 12:50:45 +0500

    changes

--- controllers/showNotificationController.js
+++ controllers/showNotificationController.js
@@ -4,6 +4,7 @@ import { editProject } from "../models/project.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
 import mongoose from "mongoose";
 import { SendEmailUtil } from "../utils/emailsender.js";
 import { sendNotification as sendPushNotificationFirebase } from "../utils/firebase.service.js"; // Renamed
