b81f7f067e8c358b9b79fcfdb9b5df86e3b565e6
faizan <fshafqat44661@gmail.com>
2025-07-24 15:57:23 +0500

    chnges

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
     },
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "*",
+allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
   })
 );
 
