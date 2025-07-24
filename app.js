e88ac6eb633725855e1a66b0f583405a1e3ffb27
faizan <fshafqat44661@gmail.com>
2025-07-24 13:21:33 +0500

    cors

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
-const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://appsoapro.techbytech.tech",
+      "https://api.appsoapro.techbytech.tech",
+      "https://appsoapro.serveng.ao"
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
